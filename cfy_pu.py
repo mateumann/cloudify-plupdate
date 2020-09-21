dc1f08a6d71f75f89fd961a69489fc4e9b924fbc
Mateusz Neumann <mateumann@gmail.com>
2020-09-21 22:57:43 +0200

    Reworking the external tool for blueprints update

--- cfy_pu.py
+++ cfy_pu.py
@@ -1,14 +1,18 @@
 #!/opt/manager/env/bin/python3
 
 import click
+import collections
 from os.path import join
 from os import environ
 from uuid import uuid4
+import yaml
 
 from dsl_parser.constants import (PLUGIN_NAME_KEY,
                                   WORKFLOW_PLUGINS_TO_INSTALL,
                                   DEPLOYMENT_PLUGINS_TO_INSTALL,
-                                  HOST_AGENT_PLUGINS_TO_INSTALL)
+                                  HOST_AGENT_PLUGINS_TO_INSTALL,
+                                  PLUGIN_PACKAGE_NAME,
+                                  PLUGIN_PACKAGE_VERSION)
 from dsl_parser.models import Plan
 
 from manager_rest.constants import FILE_SERVER_BLUEPRINTS_FOLDER
@@ -25,10 +29,16 @@ REST_CONFIG_PATH = join(REST_HOME_DIR, 'cloudify-rest.conf')
 REST_SECURITY_CONFIG_PATH = join(REST_HOME_DIR, 'rest-security.conf')
 REST_AUTHORIZATION_CONFIG_PATH = join(REST_HOME_DIR, 'authorization.conf')
 
-PACKAGE_NAME = 'package_name'
-PACKAGE_VERSION = 'package_version'
-VERSIONS = 'versions'
+BLUEPRINT_LINE = 'blueprint_line'
+CURRENT = 'current'
+EXECUTORS = [DEPLOYMENT_PLUGINS_TO_INSTALL,
+             WORKFLOW_PLUGINS_TO_INSTALL,
+             HOST_AGENT_PLUGINS_TO_INSTALL]
+IMPORTS = 'imports'
 REPO = 'repository'
+SOURCE = 'source'
+SUGGESTED = 'suggested'
+VERSIONS = 'versions'
 
 
 def _version_to_key(version: str) -> float:
@@ -125,197 +135,263 @@ CLOUDIFY_PLUGINS = {
                          key=_version_to_key, reverse=True),
         REPO: 'https://github.com/cloudify-cosmo/tosca-vcloud-plugin',
     },
+
+    # TODO mateumann testing (remove afterwards)
+    'versioned-plugin': {
+        VERSIONS: sorted(['0.0.2', '0.0.3', '0.0.4', '0.1.0', '0.1.1', '0.1.2',
+                          '0.1.3', '0.2.0', '0.3.0', '0.3.2', '1.0.0', '1.0.1',
+                          '1.0.9'], key=_version_to_key, reverse=True),
+        REPO: 'https://github.com/mateumann/cloudify-plupdate',
+    },
 }
 
 
-def validate_no_active_updates_per_blueprint(
-        sm: storage_manager.SQLStorageManager,
-        blueprint: models.Blueprint):
-    active_updates = sm.list(
-        models.PluginsUpdate,
-        filters={'blueprint_id': blueprint.id, 'state': ACTIVE_STATES},
-        all_tenants=True,
-    )
-    if active_updates:
-        raise Exception(
-            'There are plugins updates still active, update IDs: '
-            '{0}'.format(', '.join(u.id for u in active_updates)))
+# There are some Cloudify helper functions  -->
+
+# def validate_no_active_updates_per_blueprint(
+#         sm: storage_manager.SQLStorageManager,
+#         blueprint: models.Blueprint):
+#     active_updates = sm.list(
+#         models.PluginsUpdate,
+#         filters={'blueprint_id': blueprint.id, 'state': ACTIVE_STATES},
+#         all_tenants=True,
+#     )
+#     if active_updates:
+#         raise Exception(
+#             'There are plugins updates still active, update IDs: '
+#             '{0}'.format(', '.join(u.id for u in active_updates)))
+#
+#
+# def did_plugins_to_install_change(temp_plan: Plan, plan: Plan) -> bool:
+#     # Maintaining backward comparability for older blueprints
+#     if not plan.get(HOST_AGENT_PLUGINS_TO_INSTALL):
+#         plan[HOST_AGENT_PLUGINS_TO_INSTALL] = \
+#             utils.extract_host_agent_plugins_from_plan(plan)
+#     return any(
+#         did_executor_plugins_to_install_change(temp_plan, plan, executor)
+#         for executor in EXECUTORS)
+#
+#
+# def did_executor_plugins_to_install_change(
+#         temp_plan: Plan, plan: Plan, plugins_executor) -> bool:
+#     temp_plugins = temp_plan[plugins_executor]
+#     current_plugins = plan[plugins_executor]
+#     name_to_plugin = {p[PLUGIN_NAME_KEY]: p for p in current_plugins}
+#     return any(plugin for plugin in temp_plugins
+#                if plugin != name_to_plugin.get(plugin[PLUGIN_NAME_KEY], None))
+#
+#
+# def get_reevaluated_plan(rm: resource_manager.ResourceManager,
+#                          blueprint: models.Blueprint):
+#     blueprint_dir = join(
+#         config.instance.file_server_root,
+#         FILE_SERVER_BLUEPRINTS_FOLDER,
+#         blueprint.tenant.name,
+#         blueprint.id)
+#     temp_plan = rm.parse_plan(blueprint_dir,
+#                               blueprint.main_file_name,
+#                               config.instance.file_server_root)
+#     return temp_plan
+#
+#
+# def stage_plugin_update(sm: storage_manager.SQLStorageManager,
+#                         blueprint: models.Blueprint) -> models.PluginsUpdate:
+#     update_id = str(uuid4())
+#     plugins_update = models.PluginsUpdate(
+#         id=update_id,
+#         created_at=utils.get_formatted_timestamp(),
+#         forced=False)
+#     plugins_update.set_blueprint(blueprint)
+#     return sm.put(plugins_update)
+#
+#
+# def create_temp_blueprint_from(sm: storage_manager.SQLStorageManager,
+#                                rm: resource_manager.ResourceManager,
+#                                blueprint: models.Blueprint,
+#                                temp_plan) -> models.Blueprint:
+#     temp_blueprint_id = str(uuid4())
+#     kwargs = {
+#         'application_file_name': blueprint.main_file_name,
+#         'blueprint_id': temp_blueprint_id,
+#         'plan': temp_plan
+#     }
+#     # Make sure not to pass both private resource and visibility
+#     visibility = blueprint.visibility
+#     if visibility:
+#         kwargs['visibility'] = visibility
+#         kwargs['private_resource'] = None
+#     else:
+#         kwargs['visibility'] = None
+#         kwargs['private_resource'] = blueprint.private_resource
+#     temp_blueprint = rm.publish_blueprint_from_plan(**kwargs)
+#     temp_blueprint.is_hidden = True
+#     return sm.update(temp_blueprint)
+
+# These were some Cloudify helper functions  <--
 
 
-def did_plugins_to_install_change(temp_plan, plan) -> bool:
-    # Maintaining backward comparability for older blueprints
-    if not plan.get(HOST_AGENT_PLUGINS_TO_INSTALL):
-        plan[HOST_AGENT_PLUGINS_TO_INSTALL] = \
-            utils.extract_host_agent_plugins_from_plan(plan)
-    return any(
-        did_executor_plugins_to_install_change(temp_plan, plan, executor)
-        for executor in [DEPLOYMENT_PLUGINS_TO_INSTALL,
-                         WORKFLOW_PLUGINS_TO_INSTALL,
-                         HOST_AGENT_PLUGINS_TO_INSTALL])
-
-
-def did_executor_plugins_to_install_change(
-        temp_plan, plan, plugins_executor) -> bool:
-    temp_plugins = temp_plan[plugins_executor]
-    current_plugins = plan[plugins_executor]
-    name_to_plugin = {p[PLUGIN_NAME_KEY]: p for p in current_plugins}
-    return any(plugin for plugin in temp_plugins
-               if plugin != name_to_plugin.get(plugin[PLUGIN_NAME_KEY], None))
-
-
-def get_reevaluated_plan(rm: resource_manager.ResourceManager,
-                         blueprint: models.Blueprint):
-    blueprint_dir = join(
+def blueprint_file_name(blueprint: models.Blueprint) -> str:
+    return join(
         config.instance.file_server_root,
         FILE_SERVER_BLUEPRINTS_FOLDER,
         blueprint.tenant.name,
-        blueprint.id)
-    temp_plan = rm.parse_plan(blueprint_dir,
-                              blueprint.main_file_name,
-                              config.instance.file_server_root)
-    return temp_plan
+        blueprint.id,
+        blueprint.main_file_name)
 
 
-def stage_plugin_update(sm: storage_manager.SQLStorageManager,
-                        blueprint: models.Blueprint) -> models.PluginsUpdate:
-    update_id = str(uuid4())
-    plugins_update = models.PluginsUpdate(
-        id=update_id,
-        created_at=utils.get_formatted_timestamp(),
-        forced=False)
-    plugins_update.set_blueprint(blueprint)
-    return sm.put(plugins_update)
+# def get_plugin_version(name: str, version: str) -> str:
+#     if name not in CLOUDIFY_PLUGINS:
+#         return version
+#     return CLOUDIFY_PLUGINS[name][VERSIONS][0]
+#
+#
+# def suggest_plugin_versions(plugins: list) -> dict:
+#     suggestions = {}
+#     for plugin in plugins:
+#         suggestions[plugin[PLUGIN_PACKAGE_NAME]] = {
+#             OLD: plugin[PLUGIN_PACKAGE_VERSION],
+#             SUGGESTED: get_plugin_version(
+#                 plugin[PLUGIN_PACKAGE_NAME], plugin[PLUGIN_PACKAGE_VERSION]),
+#         }
+#     return suggestions
 
 
-def create_temp_blueprint_from(sm: storage_manager.SQLStorageManager,
-                               rm: resource_manager.ResourceManager,
-                               blueprint: models.Blueprint,
-                               temp_plan) -> models.Blueprint:
-    temp_blueprint_id = str(uuid4())
-    kwargs = {
-        'application_file_name': blueprint.main_file_name,
-        'blueprint_id': temp_blueprint_id,
-        'plan': temp_plan
-    }
-    # Make sure not to pass both private resource and visibility
-    visibility = blueprint.visibility
-    if visibility:
-        kwargs['visibility'] = visibility
-        kwargs['private_resource'] = None
-    else:
-        kwargs['visibility'] = None
-        kwargs['private_resource'] = blueprint.private_resource
-    temp_blueprint = rm.publish_blueprint_from_plan(**kwargs)
-    temp_blueprint.is_hidden = True
-    return sm.update(temp_blueprint)
+# def plugin_source(name: str, version: str) -> str:
+#     if name not in CLOUDIFY_PLUGINS:
+#         return None
+#     return CLOUDIFY_PLUGINS[name][REPO] + f'/archive/{version}.zip'
 
 
-def list_plugins_in_a_plan(temp_plan: Plan) -> list:
-    return [plugin
-            for plugin in temp_plan['deployment_plugins_to_install'] +
-            temp_plan['workflow_plugins_to_install'] +
-            temp_plan['host_agent_plugins_to_install']
-            if plugin[PACKAGE_NAME] and plugin[PACKAGE_VERSION]]
-
-
-def get_plugin_version(name: str, version: str, minor: bool) -> str:
-    if name not in CLOUDIFY_PLUGINS:
-        return version
-    if minor:
-        major_version = version.split('.')[0]
-        return next(v for v in CLOUDIFY_PLUGINS[name][VERSIONS]
-                    if v.startswith(major_version))
-    return CLOUDIFY_PLUGINS[name][VERSIONS][0]
-
-
-def suggest_plugin_versions(plugins: list,
-                            minor_only: bool,
-                            minor_except_names: tuple) -> dict:
-    suggestions = {}
+def substitude_plugins(plugins: list, plugin_suggestions: dict) -> list:
+    result = []
     for plugin in plugins:
-        suggestions[plugin[PACKAGE_NAME]] = get_plugin_version(
-            plugin[PACKAGE_NAME], plugin[PACKAGE_VERSION],
-            minor_only or plugin[PACKAGE_NAME] in minor_except_names)
-    return suggestions
+        if plugin[PLUGIN_PACKAGE_NAME] in plugin_suggestions:
+            suggestion = plugin_suggestions[plugin[PLUGIN_PACKAGE_NAME]]
+            plugin[PLUGIN_PACKAGE_VERSION] = suggestion[SUGGESTED]
+            plugin[SOURCE] = plugin_source(plugin[PLUGIN_PACKAGE_NAME],
+                                           plugin[PLUGIN_PACKAGE_VERSION])
+        result.append(plugin)
+    return result
 
 
-def reevaluate_plugins_to_be_updated(temp_plan: Plan,
-                                     minor_only: bool,
-                                     plugin_names: tuple,
-                                     minor_except_names: tuple) -> Plan:
-    plugins_installed = list_plugins_in_a_plan(temp_plan)
-    if plugin_names:
-        plugins_installed = [p for p in plugins_installed
-                             if p[PACKAGE_NAME] in plugin_names]
-    if not plugins_installed:
-        return temp_plan
-    plugins_suggested = suggest_plugin_versions(plugins_installed,
-                                                minor_only, minor_except_names)
-    print('reevaluate_plugins_to_be_updated')
-    print(f'PLUGINS INSTALLED\n\t{plugins_installed}')
-    print(f'PLUGINS SUGGESTED\n\t{plugins_suggested}')
-    return temp_plan
+# def reevaluate_plugins_to_be_updated(blueprint: models.Blueprint,
+#                                      temp_plan: Plan,
+#                                      minor_only: bool,
+#                                      plugin_names: tuple,
+#                                      minor_except_names: tuple) -> Plan:
+#     plugins_installed = list_plugins_in_a_plan(temp_plan)
+#     if plugin_names:
+#         plugins_installed = [p for p in plugins_installed
+#                              if p[PACKAGE_NAME] in plugin_names]
+#     if not plugins_installed:
+#         return temp_plan
+#     plugins_update_suggestions = suggest_plugin_versions(plugins_installed,
+#                                                          minor_only,
+#                                                          minor_except_names)
+#     if not plugins_update_suggestions:
+#         return temp_plan
+#
+#     # update plugins the plan
+#     for executor in EXECUTORS:
+#         if executor not in temp_plan:
+#             continue
+#         temp_plan[executor] = substitude_plugins(temp_plan[executor],
+#                                                  plugins_update_suggestions)
+#     return temp_plan
 
 
-def update_plugin(sm: storage_manager.SQLStorageManager,
-                  rm: resource_manager.ResourceManager,
-                  blueprint: models.Blueprint,
-                  minor_only: bool,
-                  plugin_names: tuple,
-                  minor_except_names: tuple) -> models.PluginsUpdate:
-    validate_no_active_updates_per_blueprint(sm, blueprint)
-    temp_plan = get_reevaluated_plan(rm, blueprint)
-    temp_plan = reevaluate_plugins_to_be_updated(
-        temp_plan, minor_only, plugin_names, minor_except_names)
-    update_required = did_plugins_to_install_change(temp_plan, blueprint.plan)
-    deployments_to_update = [d.id for d in
-                             sm.list(models.Deployment,
-                                     filters={'blueprint_id': blueprint.id},
-                                     sort={'id': 'asc'},
-                                     all_tenants=True).items]
-    update_required &= len(deployments_to_update) > 0
-    plugins_update = stage_plugin_update(sm, blueprint)
-    if update_required:
-        plugins_update.deployments_to_update = deployments_to_update
-        sm.update(plugins_update)
-        temp_blueprint = create_temp_blueprint_from(sm, rm, blueprint,
-                                                    temp_plan)
-        plugins_update.temp_blueprint = temp_blueprint
-        plugins_update.state = STATES.UPDATING
-        sm.update(plugins_update)
-    plugins_update.execution = rm.update_plugins(plugins_update,
-                                                 not update_required)
-    plugins_update.state = (STATES.NO_CHANGES_REQUIRED
-                            if not update_required
-                            else STATES.EXECUTING_WORKFLOW)
-    return sm.update(plugins_update)
+def plugins_in_a_plan(plan: Plan, plugin_names: tuple) -> collections.Iterable:
+    for executor in [DEPLOYMENT_PLUGINS_TO_INSTALL,
+                     WORKFLOW_PLUGINS_TO_INSTALL,
+                     HOST_AGENT_PLUGINS_TO_INSTALL]:
+        if executor not in plan:
+            continue
+        for plugin in plan[executor]:
+            if plugin_names and \
+                    plugin[PLUGIN_PACKAGE_NAME] not in plugin_names:
+                continue
+            if plugin[PLUGIN_PACKAGE_NAME] and \
+                    plugin[PLUGIN_PACKAGE_VERSION]:
+                yield plugin
+
+
+def import_line(imports: list, plugin_name: str) -> str:
+    for line in imports:
+        if plugin_name in line:
+            return line
+
+
+def suggest_version(plugin: dict, blueprint_line: str) -> str:
+
+    def get_plugin_version(name: str, version: str) -> str:
+        if name not in CLOUDIFY_PLUGINS:
+            return version
+        return CLOUDIFY_PLUGINS[name][VERSIONS][0]
+
+    return get_plugin_version(plugin[PLUGIN_PACKAGE_NAME],
+                              plugin[PLUGIN_PACKAGE_VERSION])
+
+
+def scan_blueprint(sm: storage_manager.SQLStorageManager,
+                   rm: resource_manager.ResourceManager,
+                   blueprint: models.Blueprint,
+                   plugin_names: tuple) -> dict:
+    file_name = blueprint_file_name(blueprint)
+    import pdb; pdb.set_trace()  # noqa
+    try:
+        with open(file_name, 'r') as f:
+            try:
+                imports = yaml.safe_load(f)[IMPORTS]
+            except yaml.YAMLError as ex:
+                print(f'Cannot load imports from {file_name}: {ex}')
+                return {}
+    except FileNotFoundError as ex:
+        print(f'Blueprint file {file_name} does not exist')
+        return {}
+    print(f'imports {imports}')
+    mappings = {}
+    for plugin in plugins_in_a_plan(blueprint.plan, plugin_names):
+        if plugin[PLUGIN_PACKAGE_NAME] in mappings:
+            continue
+        blueprint_line = import_line(imports, plugin[PLUGIN_PACKAGE_NAME])
+        mappings[plugin[PLUGIN_PACKAGE_NAME]] = {
+            CURRENT: plugin[PLUGIN_PACKAGE_VERSION],
+            BLUEPRINT_LINE: blueprint_line,
+            SUGGESTED: suggest_version(plugin, blueprint_line),
+        }
+    print(f'blueprint file name: {file_name}')
+    return mappings
 
 
 @click.command()
 @click.option('--tenant', default='default_tenant',
               help='Tenant name')
-@click.option('--plugin-name', multiple=True,
-              help='Plugin to update')
-@click.option('--minor', is_flag=True,
-              help='Update to the latest minor release')
-@click.option('--minor-except', multiple=True,
-              help='Update plugins to the latest minor release, except '
-                   'the plugin(s) specified as the arguments')
-def main(tenant, plugin_name, minor, minor_except):
-    if minor and minor_except:
-        raise Exception('--minor and --minor-except options are '
-                        'mutually exclusive')
+@click.option('--plugin-name', 'plugin_names',
+              multiple=True, help='Plugin(s) to update (you can provide '
+                                  'multiple --plugin-name(s).')
+@click.option('--blueprint', 'blueprint_ids',
+              multiple=True, help='Blueprint(s) to update (you can provide '
+                                  'multiple --blueprint(s).')
+@click.option('--mapping', 'mapping_file', multiple=False,
+              help='Provide a mapping file generated with ')
+@click.option('--correct', is_flag=True, default=False,
+              help='Update the blueprints using provided mapping file.')
+def main(tenant, plugin_names, blueprint_ids, mapping_file, correct):
+    if correct and not mapping_file:
+        raise Exception('Blueprints modification (--correct) is possible '
+                        'only with a mapping file provided with --mapping '
+                        'parameter.')
 
     set_tenant_in_app(get_tenant_by_name(tenant))
     _sm = get_storage_manager()
     _rm = get_resource_manager(_sm)
-    # import pdb; pdb.set_trace()  # noqa
-    blueprints = _sm.list(models.Blueprint,
-                          filters={'id': 'bp'},
-                          all_tenants=True)
+    filters = {'id': blueprint_ids} if blueprint_ids else None
+    #          = _sm.list(models.Blueprint, filters=filters, all_tenants=True)
+    blueprints = _sm.list(models.Blueprint, filters=filters)
     for b in blueprints.items:
         print(f'Processing {b.id} blueprint')
-        update_plugin(_sm, _rm, b, minor, plugin_name, minor_except)
+        mapping = scan_blueprint(_sm, _rm, b, plugin_names)
+        print(f'mapping {mapping}')
 
 
 if __name__ == '__main__':
